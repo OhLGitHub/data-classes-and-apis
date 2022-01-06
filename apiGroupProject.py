0b02c72fa4fa60d5c166d1243f5d118774a6e073
Annie Kittendorf <akittendorf@dev-10.com>
2022-01-06 14:28:34 -0500

    update API endpoints and dataclasses

--- apiGroupProject.py
+++ apiGroupProject.py
@@ -19,34 +19,41 @@ class Artist:
     title: str
 
 @dataclass
-class Type:
+class Exhibition:
     id: int
     title: str
     
 @dataclass
-class Style:
+class Place:
     id: int
     title: str
+    type: str
+    
 
 @dataclass
 class Gallery:
     id: int
     title: str
+    type: str
 
-json_data = requests.get('https://api.artic.edu/api/v1/artworks').json()
+art_data = requests.get('https://api.artic.edu/api/v1/artworks').json()
+artist_data = requests.get('https://api.artic.edu/api/v1/artists').json()
+place_data = requests.get('https://api.artic.edu/api/v1/places').json()
+gallery_data = requests.get('https://api.artic.edu/api/v1/galleries').json()
+exhibition_data = requests.get('https://api.artic.edu/api/v1/exhibitions').json()
 # level 0
-def populate(thing, json_data, keys): # thing is a class, json_data is json dictionary, keys is a list, returns a list of thing instances
+def populate(thing, api_data, keys): # thing is a class, json_data is json dictionary, keys is a list, returns a list of thing instances
     length = len(keys)
     data = []
     class_list = []
-    for x in json_data.keys(): # iterate through level 0 keys
+    for x in api_data.keys(): # iterate through level 0 keys
         if x == 'data': 
             # level 1
-            for i in range(0, len(json_data['data'])): # json_data['data'] is a list of dictionaries
-                for y in json_data['data'][i]:
+            for i in range(0, len(api_data['data'])): # json_data['data'] is a list of dictionaries
+                for y in api_data['data'][i]:
                     for item in keys:
                         if y == item:
-                            data.append(json_data['data'][i][y])
+                            data.append(api_data['data'][i][y])
     # chunk data list grouped parameters
     for i in range(0, len(data), length):
         # create new class instance and append to list class_list
@@ -54,9 +61,12 @@ def populate(thing, json_data, keys): # thing is a class, json_data is json dict
     print(class_list)
     return class_list
 
-    
-populate(Art, json_data, ['id', 'title', 'artist_title'])
-populate(Artist, json_data, ['artist_id', 'artist_title'])
-populate(Gallery, json_data, ['gallery_id', 'gallery_title'])
-populate(Style, json_data, ['style_id', 'style_title'])
-populate(Type, json_data, ['artwork_type_id', 'artwork_type_title'])            
+
+populate(Art, art_data, ['id', 'title', 'artist_title'])
+populate(Artist, artist_data, ['id', 'title'])
+populate(Place, place_data, ['id', 'title', 'type'])
+populate(Gallery, gallery_data, ['id', 'title', 'type'])
+populate(Exhibition, exhibition_data, ['id', 'title'])
+
+
+           
